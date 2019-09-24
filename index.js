5db17e8f0fe251b53a8e280c3982220b101fe6ee
Claret Nnamocha <devclareo@gmail.com>
2019-09-24 20:32:47 +0100

    load

---
+++ index.js
@@ -0,0 +1,27 @@
+'use strict'
+
+var express = require('express')
+const handlebars = require('express-handlebars')
+
+var app = express()
+var port = process.env.PORT || 8081
+
+app.engine('html', handlebars({
+    defaultLayout: 'main',
+    extname: '.html'
+}))
+app.set('view engine', 'html')
+
+app.get('/', function(req, res) {
+    res.render('index')
+})
+
+app.get('/signup', function(req, res) {
+    res.render('signup', { layout: false })
+})
+
+app.use(express.static('public/'));
+
+app.listen(port, function() {
+    console.log(`Server listening on http://localhost:${port}`)
+})
